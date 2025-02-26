1bf39471effeb1039bac36a7fd311b9d90ea2977
Biel <gabrielmateuspiscke@gmail.com>
2025-02-26 14:20:48 -0300

    curso

--- atv_api/atv_api/src/main/java/model/Curso.java
+++ atv_api/atv_api/src/main/java/model/Curso.java
@@ -1,4 +1,56 @@
 package model;
 
 public class Curso {
+    private int idCurso;
+    private String nome;
+    private int nemeroSala;
+  Aluno ArrayList;
+    Professor professor;
+
+    public Curso(int idCurso, String nome, int nemeroSala, Aluno arrayList, Professor professor) {
+        this.idCurso = idCurso;
+        this.nome = nome;
+        this.nemeroSala = nemeroSala;
+        ArrayList = arrayList;
+        this.professor = professor;
+    }
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNemeroSala() {
+        return nemeroSala;
+    }
+
+    public void setNemeroSala(int nemeroSala) {
+        this.nemeroSala = nemeroSala;
+    }
+
+    public Aluno getArrayList() {
+        return ArrayList;
+    }
+
+    public void setArrayList(Aluno arrayList) {
+        ArrayList = arrayList;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
 }
