8c38b8ae04a15d9460392a43f647bb148cd9c2c7
Beltrame <eliezer.santos.tds24@gmail.com>
2025-02-26 14:23:37 -0300

    Beltrame

--- atv_api/atv_api/src/main/java/model/Aluno.java
+++ atv_api/atv_api/src/main/java/model/Aluno.java
@@ -1,4 +1,37 @@
 package model;
 
-public class Aluno {
+public class Aluno{
+    private Aluno id;
+    private String nome;
+    private String cpf;
+
+    public Aluno(Aluno id, String cpf, String nome) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public Aluno getId() {
+        return id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(Aluno id) {
+        this.id = id;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
