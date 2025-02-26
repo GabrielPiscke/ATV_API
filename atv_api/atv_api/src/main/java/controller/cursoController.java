48a98a61200ed0cd6789dd096be52c7ab25a40de
Beltrame <eliezer.santos.tds24@gmail.com>
2025-02-26 15:12:56 -0300

    atv_api

--- atv_api/atv_api/src/main/java/controller/cursoController.java
+++ atv_api/atv_api/src/main/java/controller/cursoController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import banco.cursoBd;
+import model.Aluno;
+import model.Curso;
+import java.util.List;
+
 public class cursoController {
+    cursoBd repository = new cursoBd();
+
+    public List<Curso> getall() {
+        return repository.getById();
+    }
+
+    public Aluno getById(int id) {
+        return repository.findAll();
+    }
+
+    public String insert(Curso curso) {
+        return repository.insert(curso);
+    }
+
+    public Curso update(int id, Curso curso) {
+        boolean resultado = repository.upadte(id, Curso);
+
+
+        if (resultado) {
+            return null;
+        }
+        return null;
+    }
 }
